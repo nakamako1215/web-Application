136f817f15274d786e515088d1500f26321c3535
nakamako1215 <22111254mn@tama.ac.jp>
2025-05-02 23:45:32 +0900

    セッティングを変更＆迷路を関数内関数で作成

--- python_lesson/meiro.py
+++ python_lesson/meiro.py
@@ -1,5 +1,26 @@
+
 meize = [0,1,2]  # meiroの迷路のサイズ
 kaiso = 0 # 現在の階層
-goal = 0 
+
+
+
+def goal(meize):
+    miti = int(input("右=0 左=1 前=2 ->"))
+    def kaisoidou(kaiso):
+        if meize[kaiso] == miti:
+            kaiso += 1
+            print(f"正解！現在{kaiso}階層")
+        else:
+            kaiso = 0 
+        return kaiso
+    return kaisoidou(kaiso)   
+
 while True:
+    if len(meize) == kaiso:
+        break
+    kaiso = goal(meize)
     
+    if kaiso == 0:
+        print("振り出しに戻る")
+            
+print("脱出成功")
