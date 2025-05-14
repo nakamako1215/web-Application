514fc2c3f5979c33631144d9b23708d8aa0cc01c
nakamako1215 <22111254mn@tama.ac.jp>
2025-05-14 21:49:28 +0900

    エッジ処理をカラー画像に変更して合成処理

--- opencv_lesson/ezzi.py
+++ opencv_lesson/ezzi.py
@@ -1,18 +1,23 @@
 import cv2
 
 img = cv2.imread('lena.jpg')
-
-
-
+dst = cv2.Canny(img,100,100)
+dst = cv2.cvtColor(dst,cv2.IMREAD_COLOR)
+print(img.shape)
+print(dst.shape)
+gousei = cv2.addWeighted(img,0.6, dst, 0.4,0.0)
+gousei = cv2.cvtColor(img,cv2.IMREAD_COLOR)
 def onMouse(event,x,y,flags,params):
-    global pt1 , img
+    global img
     if event == cv2.EVENT_MOUSEMOVE :
-        dst = cv2.Canny(img,x,y)
+        
+        
+        print(x)
         cv2.imshow('img',dst)
         
     
 
-cv2.imshow('img',img)
+cv2.imshow('img',gousei)
 cv2.setMouseCallback('img',onMouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
