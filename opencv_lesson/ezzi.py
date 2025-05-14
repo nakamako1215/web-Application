4089fee18aaf37147907ca65b9fcb9342ad8c065
nakamako1215 <22111254mn@tama.ac.jp>
2025-05-14 21:20:57 +0900

    エッジ処理間違った実装

--- opencv_lesson/ezzi.py
+++ opencv_lesson/ezzi.py
@@ -2,16 +2,17 @@ import cv2
 
 img = cv2.imread('lena.jpg')
 
-dst = cv2.Canny(img,100,100)
+
 
 def onMouse(event,x,y,flags,params):
     global pt1 , img
     if event == cv2.EVENT_MOUSEMOVE :
-        pt1 = x
-        print(pt1)
+        dst = cv2.Canny(img,x,y)
+        cv2.imshow('img',dst)
+        
     
 
-cv2.imshow('img',dst)
+cv2.imshow('img',img)
 cv2.setMouseCallback('img',onMouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
