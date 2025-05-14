d4e933a554dc4dd834880ce326906f3eef8dd124
nakamako1215 <22111254mn@tama.ac.jp>
2025-05-14 21:00:47 +0900

    mouseeventの追加

--- opencv_lesson/ezzi.py
+++ opencv_lesson/ezzi.py
@@ -4,7 +4,14 @@ img = cv2.imread('lena.jpg')
 
 dst = cv2.Canny(img,100,100)
 
-cv2.imshow('img',dst)
+def onMouse(event,x,y,flags,params):
+    global pt1 , img
+    if event == cv2.EVENT_MOUSEMOVE :
+        pt1 = x
+        print(pt1)
+    
 
+cv2.imshow('img',dst)
+cv2.setMouseCallback('img',onMouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
