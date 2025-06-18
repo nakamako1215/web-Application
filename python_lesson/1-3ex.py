cb377ccf2865e6c3df4ca213ad4b089379423f65
nakamako1215 <22111254mn@tama.ac.jp>
2025-06-18 22:46:27 +0900

    classの練習pathlibの確認など

--- python_lesson/1-3ex.py
+++ python_lesson/1-3ex.py
@@ -6,11 +6,25 @@ def f3_1(text,startswith):
     return ans
 
 assert f3_1("12345678","3") == "345678"
-
 def f3_2(text):
-    ans = text.find("社")
-    result = text[:ans+1]+'-'+text[ans+1:]
-    print(result)
-    return result
-
+    if "株式会社" in text:
+        index = text.find("株式会社")
+        before = text[:index]
+        after = text[index + len("株式会社"):]
+        
+        if before and after:
+            return before + '-' + "株式会社" + '-' + after
+        elif before:
+            return before + '-' + "株式会社"
+        elif after:
+            return "株式会社" + '-' + after
+        else:
+            return "株式会社"
+    else:
+        return text
 assert f3_2("株式会社A") == "株式会社-A"
+
+def f3_3(li):
+    return [f"{x:.2f}" for x in li]
+       
+     
